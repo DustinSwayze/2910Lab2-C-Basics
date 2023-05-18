ade9afdc77280b8472e7cf419bd7d3ee54cbea88
Dustin Swayze <ZDCS16@ETSU.EDU>
2023-05-18 01:40:59 -0400

    commit q3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Lab2_C__Basics
+﻿using System.Data;
+using System.Runtime.InteropServices;
+using ConsoleTables;
+
+namespace Lab2_C__Basics
 {
     internal class Program
     {
@@ -7,6 +11,8 @@
             AddTwoNumbers();
             NextStep();
             MultiTable();
+            NextStep();
+            TypeTable();
         }
 
         public static void NextStep()
@@ -46,6 +52,48 @@
             }
         }
 
+        public static void TypeTable()
+        {
+            DisplayNumberTypeInfo<sbyte>("sbyte");
+            DisplayNumberTypeInfo<byte>("byte");
+            DisplayNumberTypeInfo<short>("short");
+            DisplayNumberTypeInfo<ushort>("ushort");
+            DisplayNumberTypeInfo<int>("int");
+            DisplayNumberTypeInfo<uint>("uint");
+            DisplayNumberTypeInfo<long>("long");
+            DisplayNumberTypeInfo<ulong>("ulong");
+            DisplayNumberTypeInfo<float>("float");
+            DisplayNumberTypeInfo<double>("double");
+            DisplayNumberTypeInfo<decimal>("decimal");
+        }
+
+        static void DisplayNumberTypeInfo<T>(string typeName) where T : struct
+        {
+            int sizeInBytes = GetSizeOf<T>();
+            T minValue = (T)Convert.ChangeType(GetMinValue<T>(), typeof(T));
+            T maxValue = (T)Convert.ChangeType(GetMaxValue<T>(), typeof(T));
+
+            var table = new ConsoleTable("Type", "Size (Bytes)", "Min Value", "Max Value");
+            table.AddRow(typeName, sizeInBytes, minValue, maxValue);
+            table.Write(Format.Alternative);
+        }
+
+        static int GetSizeOf<T>()
+        {
+            return Marshal.SizeOf(typeof(T));
+        }
+
+        static T GetMinValue<T>()
+        {
+            Type underlyingType = Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
+            return (T)underlyingType.GetField("MinValue").GetValue(null);
+        }
+
+        static T GetMaxValue<T>()
+        {
+            Type underlyingType = Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
+            return (T)underlyingType.GetField("MaxValue").GetValue(null);
+        }
 
     }
 }
